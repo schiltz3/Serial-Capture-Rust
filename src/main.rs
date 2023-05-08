00f8f050de948185210643bed19cc03802790c36
John Schiltz <johnschiltz2000@gmail.com>
2023-05-07 21:10:59 -0500

    Update to rust 2021

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,67 @@
-use std::path::PathBuf;
-use structopt::StructOpt;
-use dialoguer::{
-    Select,
-    theme::{ColorfulTheme, Theme},
-};
+use clap::Parser;
 use console::Term;
-use std::format;
-use enum_iterator::IntoEnumIterator;
-use std::fs::File;
+use dialoguer::{
+    theme::{ColorfulTheme, Theme},
+    Select,
+};
+use log::{error, info};
+use std::{format, fs::File, path::PathBuf};
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
 
 //
-#[derive(StructOpt, Debug)] 
-struct Args{
+#[derive(Debug, Parser)]
+struct Args {
     // a flag, true if used in the command line
-
-    #[structopt(short, long)]
+    #[arg(short, long)]
     dubug: bool,
 
-    // The number of occurerences of the 'v/verbose' flag
-    #[structopt(short, long, parse(from_occurrences))]
-    verbose: u8,
+    #[command(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
 
     // Select Serial port
-    #[structopt(short = "p", long = "port", requires = "baud")]
+    #[arg(short, long = "port", requires = "baud")]
     port: Option<String>,
 
-    #[structopt(short = "b", long = "baud", requires = "port")]
+    #[arg(short, long = "baud", requires = "port")]
     baud: Option<u8>,
 
     // Select config file
-    #[structopt(short = "c", long = "config", parse(from_os_str), default_value = "config.yaml")]
+    #[arg(short, long = "config", default_value = "config.yaml")]
     config_file: PathBuf,
     //
     // Output file
-    #[structopt(short = "o", long = "output", parse(from_os_str), default_value = "output.csv")]
+    #[arg(short, long = "output", default_value = "output.csv")]
     output_file: PathBuf,
 }
 
 // Takes upened file and writes standard config info to it.
-fn create_config(output: File) -> File{
-  //todo put in yaml code to make basic config  
+fn create_config(output: File) -> File {
+    //todo put in yaml code to make basic config
     println!("Create_config");
     output
 }
 
-fn is_valid_config(config: File) -> bool{
-
+fn is_valid_config(config: File) -> bool {
     false
 }
 
-fn main() -> std::io::Result<()>{
-
+fn main() -> std::io::Result<()> {
     let term = Term::stdout();
-    let args = Args::from_args();
+    let args = Args::parse();
 
     // Open/create config file
     let config = match File::open(&args.config_file) {
         Ok(config) => {
-            print!("OK");
+            info!("Config file found");
             config
         }
         Err(_) => {
+            info!("Creating config file");
             File::create(&args.config_file)?;
             //create_config(File::open(&args.config_file)?)
             File::open(&args.config_file).map(create_config)?
-        }
-        //set config menue flag
+        } //set config menu flag
     };
 
     // Open/create output file
@@ -87,11 +83,12 @@ fn main() -> std::io::Result<()>{
     // write debug macro
     // term.write_line(&opt_str)?;
 
-
-
-
-    let selection = Select::with_theme(&ColorfulTheme::default()).default(0)
-        .item(format!("Run with current config: {}", args.config_file.to_str().unwrap()))
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .default(0)
+        .item(format!(
+            "Run with current config: {}",
+            args.config_file.to_str().unwrap()
+        ))
         .item("Inport config file")
         .item("Edit config file")
         .interact()?;
@@ -109,145 +106,134 @@ fn main() -> std::io::Result<()>{
 }
 
 #[derive(Default)]
-struct State{
+struct State {
     port: Option<String>,
     baud: Option<String>,
     file: Option<String>,
 }
 
-
-#[derive(IntoEnumIterator, PartialEq, Eq)]
+#[derive(PartialEq, Eq, EnumIter)]
 #[repr(usize)]
-enum Options{
+enum Options {
     Port = 0,
     Baud,
     File,
     Exit,
 }
-impl Options{
-    fn map(&self, state: &State) -> String{
+impl Options {
+    fn map(&self, state: &State) -> String {
         use Options::*;
         match self {
-            Port => {
-                state.port
-                    .as_ref()
-                    .map(|p| format!("Change current port: ({})", p))
-                    .unwrap_or_else(|| format!("Set port"))
-            }
-            Baud => {
-                state.baud
-                    .as_ref()
-                    .map(|p| format!("Change current baud rate: ({})", p))
-                    .unwrap_or_else(|| format!("Set baud rate"))
-            }
-            File => {
-                state.file
-                    .as_ref()
-                    .map(|p| format!("Change output file location: ({})", p))
-                    .unwrap_or_else(|| format!("Set output file location"))
-            }
-            Exit =>{
+            Port => state
+                .port
+                .as_ref()
+                .map(|p| format!("Change current port: ({})", p))
+                .unwrap_or_else(|| format!("Set port")),
+            Baud => state
+                .baud
+                .as_ref()
+                .map(|p| format!("Change current baud rate: ({})", p))
+                .unwrap_or_else(|| format!("Set baud rate")),
+            File => state
+                .file
+                .as_ref()
+                .map(|p| format!("Change output file location: ({})", p))
+                .unwrap_or_else(|| format!("Set output file location")),
+            Exit => {
                 format!("Save")
             }
         }
     }
 
-    fn make_selection<'s,'t>(state: &'s State, theme: &'t dyn Theme) -> Select<'t>{
+    fn make_selection<'s, 't>(state: &'s State, theme: &'t dyn Theme) -> Select<'t> {
         let mut selection = Select::with_theme(theme);
 
         selection.default(0);
 
-        Self::into_enum_iter()
+        Self::iter()
             // Set each one to their value based off state
             .map(|s| s.map(&state))
             // Add each item into selection
-            .fold(selection, |mut s,a| {s.item(a); s})
+            .fold(selection, |mut s, a| {
+                s.item(a);
+                s
+            })
     }
 
-    fn update_state(options: usize, state: &mut State, term: &Term) -> bool{
-        if options == Self::Port as usize{
-                term.write_line("Enter port name: ").ok();
-                term.move_cursor_up(1).ok();
-                term.move_cursor_right(17).ok();
-                state.port = term.read_line().ok();
+    fn update_state(options: usize, state: &mut State, term: &Term) -> bool {
+        if options == Self::Port as usize {
+            term.write_line("Enter port name: ").ok();
+            term.move_cursor_up(1).ok();
+            term.move_cursor_right(17).ok();
+            state.port = term.read_line().ok();
+        } else if options == Self::Baud as usize {
+            term.write_line("Enter Baud Rate: ").ok();
+            term.move_cursor_up(1).ok();
+            term.move_cursor_right(17).ok();
+            state.baud = term.read_line().ok();
+        } else if options == Self::File as usize {
+            term.write_line("Enter File Path: ").ok();
+            term.move_cursor_up(1).ok();
+            term.move_cursor_right(17).ok();
+            state.file = term.read_line().ok();
+        } else if options == Self::Exit as usize {
+            return false;
+        } else {
+            unreachable!("Outside of menue");
         }
-        else if options == Self::Baud as usize{
-                term.write_line("Enter Baud Rate: ").ok();
-                term.move_cursor_up(1).ok();
-                term.move_cursor_right(17).ok();
-                state.baud = term.read_line().ok();
-        }
-        else if options == Self::File as usize{
-                term.write_line("Enter File Path: ").ok();
-                term.move_cursor_up(1).ok();
-                term.move_cursor_right(17).ok();
-                state.file = term.read_line().ok();
-        }
-        else if options == Self::Exit as usize{return false;}
-        else {unreachable!("Outside of menue");}
         true
     }
 }
 
-fn make_config(term: &Term) -> std::io::Result<()>{
-
+fn make_config(term: &Term) -> std::io::Result<()> {
     let mut state = State::default();
 
     let theme = ColorfulTheme::default();
 
     let mut should_cont = true;
 
-    while should_cont{
-
+    while should_cont {
         let opt = Options::make_selection(&state, &theme).interact()?;
 
         should_cont = Options::update_state(opt, &mut state, &term);
-
     }
 
-
-
-
-
     /*
-       let mut port = None;
-       let mut baud = None;
-       loop{
-       let selection = Select::with_theme(&ColorfulTheme::default())
-       .item(State.port)
-       .item("Set Baud rate")
-       .item("Parity bit?")
-       .item("Log file location")
-       .item("Finish config")
-       .interact()?;
+    let mut port = None;
+    let mut baud = None;
+    loop{
+    let selection = Select::with_theme(&ColorfulTheme::default())
+    .item(State.port)
+    .item("Set Baud rate")
+    .item("Parity bit?")
+    .item("Log file location")
+    .item("Finish config")
+    .interact()?;
 
-       match selection {
-       0 => {
-       term.write_line("Enter port name: ")?;
-       let mut port_t = String::new();
-       io::stdin()
-       .read_line(&mut port_t)
-       .expect("Failed to read port");
-       port_t.pop();
-       port.replace(port_t);
-       }
-       1 => {
-       term.write_line("Enter baud rate: ")?;
-       let mut baud = String::new();
-       io::stdin()
-       .read_line(&mut baud)
-       .expect("Failed to read baud");
-       }
-       4 => break,
-       _ => unreachable!("out of menue"),
-       }
+    match selection {
+    0 => {
+    term.write_line("Enter port name: ")?;
+    let mut port_t = String::new();
+    io::stdin()
+    .read_line(&mut port_t)
+    .expect("Failed to read port");
+    port_t.pop();
+    port.replace(port_t);
+    }
+    1 => {
+    term.write_line("Enter baud rate: ")?;
+    let mut baud = String::new();
+    io::stdin()
+    .read_line(&mut baud)
+    .expect("Failed to read baud");
+    }
+    4 => break,
+    _ => unreachable!("out of menue"),
+    }
 
-       }
-       */
+    }
+    */
     Ok(())
 }
 
-fn import_config(default: bool){
-
-}
-
+fn import_config(default: bool) {}
